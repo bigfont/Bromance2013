613c1beae5452a92c909a3630040b793d13653ec
Shaun Luttin <shaunluttin@gmail.com>
2013-11-23 14:13:36 -0800

    Complete ex7.

---
+++ starting-angular/starting-angular/cards/examples/ex07/js/controllers.js
@@ -0,0 +1,18 @@
+﻿var cardAppControllers = (function () {
+    'use strict';
+    var cardAppControllers = angular.module('cardAppControllers', []);
+
+    // Declare the application controller and inject the scope reference.
+    cardAppControllers.controller('AppCtrl', ['$scope', function ($scope) {
+        // Define the title model.
+        $scope.title = "AngularJS Tutorial";
+    }]);
+    // Inject the scope and new cardService reference into the controller.
+    cardAppControllers.controller('ListCtrl', ['$scope', 'cardService',
+                                  function ($scope, cardService) {
+                                      // Define the cards model.					  
+                                      cardService.getCards($scope);
+                                  }]);
+
+    return cardAppControllers;
+}());
