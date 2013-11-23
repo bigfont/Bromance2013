1d09b8ca846dd47076c3cdda9b4875703485b6ae
Shaun Luttin <shaunluttin@gmail.com>
2013-11-23 14:50:17 -0800

    Add example 9

---
+++ starting-angular/starting-angular/cards/examples/ex09/js/controllers.js
@@ -0,0 +1,30 @@
+﻿var cardAppControllers = (function () {
+    'use strict';
+    var cardAppControllers = angular.module('cardAppControllers', []);
+
+    // Declare the application controller and inject the scope reference.
+    cardAppControllers.controller('AppCtrl', ['$scope', function ($scope) {
+        // Define the title model.
+        $scope.title = "AngularJS Tutorial";
+    }]);
+    // Inject the scope and new cardService reference into the controller.
+    cardAppControllers.controller('ListCtrl', ['$scope', 'cardService',
+                                  function ($scope, cardService) {
+                                      // Define the cards model.					  
+                                      cardService.getCards($scope);
+                                  }]);
+
+    // Inject scope, $routeParams, and cardService  
+    cardAppControllers.controller('DetailCtrl', ['$scope', '$routeParams', 'cardService',
+        function ($scope, $routeParams, cardService) {
+            cardService.getCard($scope, $routeParams.numberID, $routeParams.suitID);
+        }]);
+
+    cardAppControllers.controller('DeleteCtrl', ['$scope', '$routeParams', 'cardService',
+        function DeleteCtrl($scope, $routeParams, cardService) {
+            cardService.deleteCard($scope, $routeParams.numberID, $routeParams.suitID);
+        }]);
+
+
+    return cardAppControllers;
+}());
