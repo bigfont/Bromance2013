2a7392ec44807ba6722d9e2e4deb8f03518625f6
Shaun Luttin <shaunluttin@gmail.com>
2013-11-23 13:23:00 -0800

    Complete example 4.

---
+++ starting-angular/starting-angular/cards/examples/ex04/js/controllers.js
@@ -0,0 +1,13 @@
+﻿function AppCtrl($scope) {
+    $scope.title = "AngularJS Hello World!";
+
+    // Define a new model called 'cards' with JSON.
+    $scope.cards = [
+        { "number": "2", "suit": "Hearts", "numOrd": 2 },
+        { "number": "10", "suit": "Spades", "numOrd": 10 },
+        { "number": "5", "suit": "Spades", "numOrd": 5 },
+        { "number": "Q", "suit": "Hearts", "numOrd": 12 }
+    ];
+}
+// Inject scope into the controller.
+AppCtrl.$inject = ['$scope'];
