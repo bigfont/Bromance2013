576187f575ceeac993b2d99c773d9229ac133003
Shaun Luttin <shaunluttin@gmail.com>
2013-11-23 16:31:35 -0800

    Add ex 13.

---
+++ starting-angular/starting-angular/cards/examples/ex13/js/controllers.js
@@ -0,0 +1,10 @@
+﻿function Ctrl($scope) {
+    $scope.userAddress = 'Vancouver';
+
+    $scope.update = function (input) {
+        alert("Updated address to: " + input);
+    };
+    $scope.cancel = function (input) {
+        alert("Cancelled changes to: " + input);
+    };
+}
